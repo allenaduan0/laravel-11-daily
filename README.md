569172a9f0187b6da909b870fed11fea43781334
Allen Aduan <business.dev.allen@gmail.com>
2026-01-20 09:12:11 +0400

    feat: add sanctum token authentication for api

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ This repository documents my daily learning journey with **Laravel 11**, focusin
 -   Day 7: Laravel 11 introduce API resource for task responses  
 -   Day 8: Laravel 11 Pagination added to tasks API
 -   Day 9: Laravel 11 Add task filtering and search using query scopes
+-   Day 10: Laravel 11 Add Sanctum token authentication for api
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Api\V1\AuthController;
 
-Route::get("/", function () {
-    return response()->json([
-        "message" => "Laravel 11 Daily – Day 1",
-        "status" => "OK",
-    ]);
+//Route::get("/", function () {
+//    return response()->json([
+//        "message" => "Laravel 11 Daily – Day 1",
+//        "status" => "OK",
+//    ]);
+//});
+//
+//Route::get("/tasks", [TaskController::class, "index"]);
+//Route::post("/tasks", [TaskController::class, "store"]);
+//Route::put("/tasks/{task}", [TaskController::class, "update"]);
+//Route::delete("/tasks/{task}", [TaskController::class, "destroy"]);
+
+Route::prefix('api/v1')->middleware('api')->group(function () {
+   Route::post('/login', [AuthController::class, 'login']);
+
+   Route::middleware('auth:sanctum')->group(function () {
+       Route::apiResource('tasks', TaskController::class);
+   });
 });
-
-Route::get("/tasks", [TaskController::class, "index"]);
-Route::post("/tasks", [TaskController::class, "store"]);
-Route::put("/tasks/{task}", [TaskController::class, "update"]);
-Route::delete("/tasks/{task}", [TaskController::class, "destroy"]);
